641a8c80787cf30fca7cd7fe97762850611be0fb
zz <xzrunner@gmail.com>
2020-02-07 08:30:41 +0800

    [ADDED] Line3D

---
+++ include/geoshape/Line3D.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "geoshape/Shape3D.h"
+
+namespace gs
+{
+
+class Line3D : public Shape3D
+{
+public:
+	Line3D() {}
+	Line3D(const sm::vec3& start, const sm::vec3& end);
+
+	virtual std::unique_ptr<Shape3D> Clone() const override;
+
+	auto& GetStart() const { return m_start; }
+    auto& GetEnd()   const { return m_end; }
+	void SetStart(const sm::vec3& start);
+    void SetEnd(const sm::vec3& end);
+
+private:
+	void BuildBounding();
+
+private:
+	sm::vec3 m_start, m_end;
+
+	RTTR_ENABLE(Shape3D)
+
+}; // Line3D
+
+}
---
+++ source/Line3D.cpp
@@ -0,0 +1,52 @@
+#include "geoshape/Line3D.h"
+#include "geoshape/config.h"
+
+#include <SM_Calc.h>
+
+RTTR_REGISTRATION
+{
+
+rttr::registration::class_<gs::Line3D>("gs_line3d")
+	.constructor<>()
+	.constructor<const sm::vec3&, const sm::vec3&>()
+	.property("start", &gs::Line3D::GetStart, &gs::Line3D::SetStart)
+    .property("end", &gs::Line3D::GetEnd, &gs::Line3D::SetEnd)
+;
+
+}
+
+namespace gs
+{
+
+Line3D::Line3D(const sm::vec3& start, const sm::vec3& end)
+	: m_start(start)
+    , m_end(end)
+{
+	BuildBounding();
+}
+
+std::unique_ptr<Shape3D> Line3D::Clone() const
+{
+	return std::make_unique<Line3D>(m_start, m_end);
+}
+
+void Line3D::SetStart(const sm::vec3& start)
+{
+	m_start = start;
+	BuildBounding();
+}
+
+void Line3D::SetEnd(const sm::vec3& end)
+{
+	m_end = end;
+	BuildBounding();
+}
+
+void Line3D::BuildBounding()
+{
+    m_bounding.MakeEmpty();
+    m_bounding.Combine(m_start);
+    m_bounding.Combine(m_end);
+}
+
+}
